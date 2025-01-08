f0e4540572f69ee495cbadfe0f50bd435187b4aa
NicolasTennberg <tennberg.git@gmail.com>
2025-01-08 11:23:01 +0100

    Login and Tests for signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    include SessionsHelper
     # prevents x-site request forgery
     protect_from_forgery with: :exception
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
       # Log the user in and redirect to the user's show page.
     else
-      flash[:danger] = 'Invalid email/password combination' #Not quite right!
-    render 'new', status: :unprocessable_entity
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
     end
   end
 
