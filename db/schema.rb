48a0c70b5b74276bb839bb55ce89c7742a1fac01
NicolasTennberg <tennberg.git@gmail.com>
2025-01-13 15:32:30 +0100

    Account activation and some tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_13_102513) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_13_130010) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,6 +19,11 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_13_102513) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
+    t.string "rails"
+    t.string "activation_digest"
+    t.string "string"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
