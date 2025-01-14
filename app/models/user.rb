3fae645e91f96f5507f5e78059197bb106ede448
NicolasTennberg <tennberg.git@gmail.com>
2025-01-14 11:48:39 +0100

    Account activation and edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,25 +10,24 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
-    class << self
-      # Returns the hash digest of the given string.
-      def digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
-      end
-  
-      # Returns a random token.
-      def new_token
-        SecureRandom.urlsafe_base64
-      end
-    end
-  
-    # Remembers a user in the database for use in persistent sessions.
-    def remember
-        self.remember_token = User.new_token
-        update_attribute(:remember_digest, User.digest(remember_token))
-        remember_digest
-      end
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
+  end
 
   # Returns a session token to prevent session hijacking.
   # We reuse the remember digest for convenience.
@@ -37,9 +36,10 @@ class User < ApplicationRecord
   end
   
     # Returns true if the given token matches the digest.
-    def authenticated?(remember_token)
-      return false if remember_digest.nil?
-      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    def authenticated?(attribute, token)
+        digest = send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
     end
       
     # Forgets a user.
