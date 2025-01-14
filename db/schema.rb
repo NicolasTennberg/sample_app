6fa2a85db8acf186f97f8be84f8dd52b7f789317
NicolasTennberg <tennberg.git@gmail.com>
2025-01-14 15:04:31 +0100

    Password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_13_130010) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_14_125825) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_13_130010) do
     t.string "string"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
